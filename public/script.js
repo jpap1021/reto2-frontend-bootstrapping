5c045a44baa22e8fdfc57cb77c42793c6b5d63bc
jpap1021 <juan.arteaga8710@gmail.com>
2019-02-07 16:07:41 -0500

    home ready

--- public/script.js
+++ public/script.js
@@ -1,8 +1,25 @@
-fetch("https://randomuser.me/api/?results=2000").then((value) =>{   
+fetch("https://randomuser.me/api/?results=100").then((value) =>{   
 return value.json();
 }).then((value) =>{
-    console.log(value)
-    value.results.forEach(function(result) {
-        console.log(result)
+  console.log(value)
+ 
+  value.results.forEach(function(result) {
+      resultado(result)
     });
 });
+
+let container = document.getElementById('container')
+
+function resultado(personaje){
+
+  let div = document.createElement('div');
+  let span = document.createElement('span');
+  let imagen = document.createElement('img');
+  imagen.src= personaje.picture.large;
+  span.textContent = `${personaje.name.first}  ${personaje.name.first} `;
+  div.appendChild(span)
+  div.appendChild(imagen)
+  div.classList.add('column');
+  container.appendChild(div);
+  container.classList.add('wrap');
+}
